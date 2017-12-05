f39ec7f5fcfb9fee0862801ff88fb651094c6237
taetrainingryan <33837187+taetrainingryan@users.noreply.github.com>
2017-12-05 19:07:54 +0000

    initial commit.

--- app/src/main/java/com/example/ryan/flavoursdemo/services/API_List.java
+++ app/src/main/java/com/example/ryan/flavoursdemo/services/API_List.java
@@ -1,10 +1,12 @@
 package com.example.ryan.flavoursdemo.services;
 
+import com.example.ryan.flavoursdemo.BuildConfig;
+
 /**
  * Created by Ryan on 05/12/2017.
  */
 
 public class API_List {
 
-    public static final String BASE_URL = "http://api.duckduckgo.com/?q=family+guy+characters&format=json";
+    public static final String BASE_URL = "http://" + BuildConfig.BASE_URL;
 }
